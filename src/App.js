efcb36989404e098242d03070c4e8d88dc077cf8
prashant naik <real.hyper.clocked@gmail.com>
2022-01-05 19:10:03 +0530

    updating theme

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import { Provider } from "react-redux";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ProviderContainer from "./components/ProviderContainer/ProviderContainer";
 import ProviderServices from "./components/ProviderServices/ProviderServices";
 import store from "./store";
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+    secondary: {
+      main: red[200],
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <div>
-          <NavBar />
-          <main>
-            <Routes>
-              <Route path="/" element={<ProviderContainer />}></Route>
-            </Routes>
-          </main>
-          <main>
-            <Routes>
-              <Route path="/services" element={<ProviderServices />}></Route>
-            </Routes>
-          </main>
-        </div>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <div>
+            <NavBar />
+            <main>
+              <Routes>
+                <Route path="/" element={<ProviderContainer />}></Route>
+              </Routes>
+            </main>
+            <main>
+              <Routes>
+                <Route path="/services" element={<ProviderServices />}></Route>
+              </Routes>
+            </main>
+          </div>
+        </Provider>
+      </ThemeProvider>
     </div>
   );
 }
